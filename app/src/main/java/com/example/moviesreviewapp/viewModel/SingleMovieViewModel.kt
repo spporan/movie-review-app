0d03b87acb37b34efbcc63838b4db96cce00cd26
poran <paran.cse@gmail..com>
2019-10-20 14:23:17 +0600

    create Single movie viewModel SingleMovieView

---
+++ app/src/main/java/com/example/moviesreviewapp/viewModel/SingleMovieViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.moviesreviewapp.viewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.example.moviesreviewapp.data.repository.MovieDetailsRepository
+import com.example.moviesreviewapp.data.repository.NetworkState
+import com.example.moviesreviewapp.data.vo.MovieDetails
+import io.reactivex.disposables.CompositeDisposable
+
+class SingleMovieViewModel (private val movieDetailsRepository: MovieDetailsRepository,movie_id:Int):ViewModel (){
+    private val compositeDisposable=CompositeDisposable()
+
+    val movieDetails:LiveData<MovieDetails> by lazy{
+        movieDetailsRepository.fetchingSignleMovieDetails(compositeDisposable,movie_id)
+
+    }
+    val networkState:LiveData<NetworkState> by lazy {
+        movieDetailsRepository.fetchNetworkstate()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
+}
