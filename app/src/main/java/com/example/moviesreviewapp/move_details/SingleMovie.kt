b72b5b0dbc2716b0fbcb289f98e3f147dd0393f7
poran <paran.cse@gmail..com>
2019-10-20 12:38:08 +0600

    add movie details page

---
+++ app/src/main/java/com/example/moviesreviewapp/move_details/SingleMovie.kt
@@ -0,0 +1,13 @@
+package com.example.moviesreviewapp.move_details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moviesreviewapp.R
+
+class SingleMovie : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_movie)
+    }
+}
